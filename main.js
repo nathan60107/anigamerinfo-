111ecae5b482128af25cd7b4a560cc39eb68a132
Nathan <nathan60107@yahoo.com.tw>
2024-04-14 13:09:55 +0800

    fix: improve cv text

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  在動畫瘋中自動擷取動畫常見相關資訊，如CAST以及主題曲。
 // @namespace    nathan60107
 // @author       nathan60107(貝果)
-// @version      1.1.1
+// @version      1.1.2
 // @homepage     https://home.gamer.com.tw/creationCategory.php?owner=nathan60107&c=425332
 // @match        https://ani.gamer.com.tw/animeVideo.php?sn=*
 // @icon         https://ani.gamer.com.tw/apple-touch-icon-144.jpg
@@ -268,7 +268,7 @@ async function getSyoboi(searchGoogle = false) {
   for (let role of castData) {
     cast.push({
       char: $(role).find('th').text(),
-      cv: $(role).find('td > a').text()
+      cv: $(role).find('td').text()
     })
   }
 
