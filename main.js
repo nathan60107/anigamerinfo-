b6e430d939747a3597703ae382268cd5f7c94368
Nathan <nathan60107@yahoo.com.tw>
2026-09-08 00:41:45 +0800

    fix: support shochiku exception site

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  在動畫瘋中自動擷取動畫常見相關資訊，如CAST以及主題曲。
 // @namespace    nathan60107
 // @author       nathan60107(貝果)
-// @version      1.2
+// @version      1.2.1
 // @homepage     https://home.gamer.com.tw/creationCategory.php?owner=nathan60107&c=425332
 // @match        https://ani.gamer.com.tw/animeVideo.php?sn=*
 // @icon         https://ani.gamer.com.tw/apple-touch-icon-144.jpg
@@ -205,7 +205,8 @@ async function searchSyoboi() {
   let exceptionSite = [
     'tv-tokyo.co.jp',
     'tbs.co.jp',
-    'sunrise-inc.co.jp'
+    'sunrise-inc.co.jp',
+    'sh-anime.shochiku.co.jp'
   ]
   if (exceptionSite.includes(site)) {
     // https://stackoverflow.com/a/33305263
